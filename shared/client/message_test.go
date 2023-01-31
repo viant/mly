082203cdd1fe251c273d668187546554deb6c772
David Choi <dchoi@viantinc.com>
2023-01-31 01:41:13 +0000

    Reproduce cache key error in unit test.

---
+++ shared/client/message_test.go
@@ -0,0 +1,62 @@
+package client
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/viant/mly/shared"
+	"github.com/viant/mly/shared/common"
+)
+
+func TestMessage(t *testing.T) {
+	commonDict := &common.Dictionary{
+		Layers: []common.Layer{
+			common.Layer{
+				Name:    "copied",
+				Strings: []string{},
+			},
+			common.Layer{
+				Name:    "multi",
+				Strings: []string{"a", "b", "c"},
+			},
+		},
+		Hash: 1,
+	}
+
+	inputs := []*shared.Field{
+		&shared.Field{
+			Name:     "copied",
+			Index:    0,
+			DataType: "string",
+			//Wildcard: true,
+		},
+		&shared.Field{
+			Name:     "multi",
+			Index:    1,
+			DataType: "string",
+		},
+	}
+
+	makeDict := func() *Dictionary {
+		dict := NewDictionary(commonDict, inputs)
+		return dict
+	}
+
+	msgs := NewMessages(makeDict)
+	msg := msgs.Borrow()
+
+	msg.SetBatchSize(3)
+
+	msg.StringsKey("multi", []string{"a", "b", "c"})
+	msg.StringsKey("copied", []string{"1"})
+
+	var key string
+	key = msg.CacheKeyAt(0)
+	assert.Equal(t, "1/a", key)
+
+	key = msg.CacheKeyAt(1)
+	assert.Equal(t, "1/b", key)
+
+	key = msg.CacheKeyAt(2)
+	assert.Equal(t, "1/c", key)
+}
