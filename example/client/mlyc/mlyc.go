48aac0513fe0054fc9a6b702dea40b894c095d19
David Choi <dchoi@viantinc.com>
2023-06-05 16:57:11 -0700

    Refactor client to include Storable registration before RunApp

--- example/client/mlyc/mlyc.go
+++ example/client/mlyc/mlyc.go
@@ -1,10 +1,28 @@
 package main
 
 import (
-	"github.com/viant/mly/example/client"
 	"os"
+
+	"github.com/viant/mly/example/client"
+	slfmodel "github.com/viant/mly/example/transformer/slf/model"
+	"github.com/viant/mly/shared/common"
+	"github.com/viant/mly/shared/common/storable"
 )
 
 func main() {
+	storableSrv := storable.Singleton()
+
+	// in actual client code, any type information should be available within the context of the caller, so using a dynamic
+	// type instantiation service is not needed
+	storableSrv.Register(slfmodel.Namespace, func() common.Storable {
+		return new(slfmodel.Segmented)
+	})
+
+	storableSrv.Register("slft_batch", func() common.Storable {
+		sa := make([]*slfmodel.Segmented, 0)
+		segmenteds := slfmodel.Segmenteds(sa)
+		return &segmenteds
+	})
+
 	client.Run(os.Args[1:])
 }
--- example/client/runner.go
+++ example/client/runner.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"fmt"
 
-	slfmodel "github.com/viant/mly/example/transformer/slf/model"
 	"github.com/viant/mly/service/endpoint/checker"
 	"github.com/viant/mly/shared/client"
-	"github.com/viant/mly/shared/common"
 	"github.com/viant/mly/shared/common/storable"
 	"github.com/viant/toolbox"
 )
@@ -47,19 +45,6 @@ func RunWithOptions(options *Options) error {
 	response := &client.Response{}
 
 	storableSrv := storable.Singleton()
-
-	// in actual client code, any type information should be available within the context of the caller, so using a dynamic
-	// type instantiation service is not needed
-	storableSrv.Register(slfmodel.Namespace, func() common.Storable {
-		return new(slfmodel.Segmented)
-	})
-
-	storableSrv.Register("slft_batch", func() common.Storable {
-		sa := make([]*slfmodel.Segmented, 0)
-		segmenteds := slfmodel.Segmenteds(sa)
-		return &segmenteds
-	})
-
 	maker, err := storableSrv.Lookup(options.Storable)
 	if err != nil {
 		if options.Debug {
