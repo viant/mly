39e1711b95928867c00a130b4ea5a2aa566224cb
David Choi <dchoi@viantinc.com>
2023-07-20 15:39:07 -0700

    Remake example models, add float input models.

--- example/model/make_mock_models.py
+++ example/model/make_mock_models.py
@@ -16,6 +16,9 @@ class LookupLayer(Layer):
         self.table_name = table_name
         super().__init__(name=name, trainable=False)
 
+    def get_config(self):
+        return dict()
+
     def build(self, input_shape):
         keys_tensor = tf.constant(self.keys)
         values_tensor = tf.constant(self.values)
@@ -38,6 +41,7 @@ if __name__ == '__main__':
 
     ap.add_argument('--no-vectorization', action='store_true')
     ap.add_argument('--no-string-lookup', action='store_true')
+    ap.add_argument('--no-float-input', action='store_true')
 
     ap.add_argument('--no-int-out', action='store_true')
     ap.add_argument('--no-float-out', action='store_true')
@@ -46,6 +50,7 @@ if __name__ == '__main__':
     ap.add_argument('--no-keyed-out', action='store_true', help='for testing v0.5.0')
 
     ap.add_argument('--no-slow', action='store_true', help='for testing thread limit')
+    ap.add_argument('--slow-repeat', type=int, default=10, help='looping math operations for slow model')
 
     pa, _ = ap.parse_known_args()
 
@@ -62,6 +67,7 @@ if __name__ == '__main__':
     test_sl_data = []
     test_tv_data = []
     test_ll_data = []
+    test_fl_data = []
 
     for ti in range(pa.num_test_samples):
         test_sl_data.append(random.choice(vocab_data))
@@ -72,6 +78,8 @@ if __name__ == '__main__':
 
         test_ll_data.append(random.randint(0, vocab_size - 1))
 
+        test_fl_data.append(random.random())
+
     test_sl_data = test_sl_data[:-1]
     test_sl_data.append(oov_element)
 
@@ -80,6 +88,7 @@ if __name__ == '__main__':
 
     test_sl_tensor = tf.constant(test_sl_data)
     test_tv_tensor = tf.constant(test_tv_data)
+    test_fl_tensor = tf.constant(test_fl_data)
 
     def inputs_with_vec():
         tv_i = Input(shape=(1,), name='tv', dtype='string')
@@ -99,12 +108,24 @@ if __name__ == '__main__':
         sl_layer = StringLookup(name='sl_string_lookup')
         sl_layer.adapt(vocab_data)
 
+        # string lookup alternate
         sa_i = Input(shape=(1,), name='sa', dtype='string')
         sa_layer = StringLookup(name='sa_string_lookup')
         sa_layer.adapt(vocab_data)
 
         o = Concatenate()([sa_layer(sa_i), sl_layer(sl_i)])
         return [sl_i, sa_i], o
+
+    def inputs_float():
+        sl_i = Input(shape=(1,), name='sl', dtype='string')
+        sl_layer = StringLookup(name='sl_string_lookup')
+        sl_layer.adapt(vocab_data)
+
+        fl_i = Input(shape=(1,), name='fl', dtype='float32')
+
+        o = tf.math.divide(sl_layer(sl_i), 7, name='sl_divide')
+        o = Concatenate()([o, fl_i])
+        return [sl_i, fl_i], o
 
     input_generators = [(
             pa.no_vectorization, 
@@ -117,6 +138,12 @@ if __name__ == '__main__':
             'string_lookups', 
             inputs_sl_only, 
             {'sa': test_sl_tensor, 'sl': test_sl_tensor}
+        ),
+        (
+            pa.no_float_input, 
+            'float_input', 
+            inputs_float, 
+            {'fl': test_fl_tensor, 'sl': test_sl_tensor}
         )]
 
     output_mods = [(pa.no_float_out, True, 'float'), 
@@ -135,7 +162,7 @@ if __name__ == '__main__':
 
         save_dir = f'{check_model.name}_model'
         print(f'Saving model to {save_dir}...')
-        check_model.save(save_dir)
+        check_model.save(save_dir, include_optimizer=False)
         print(f'Saved model to {save_dir}.')
 
     for gen_params, mod_params in itertools.product(input_generators, output_mods):
@@ -150,13 +177,14 @@ if __name__ == '__main__':
 
         inputs, o = generator()
 
-        o = Lambda(lambda t: tf.math.add(t, 1), name='add_one')(o)
-        o = Lambda(lambda t: tf.math.reduce_prod(t, axis=1), name='reduce_prod')(o)
+        o = tf.math.add(o, 1, name='add_one')
+        o = tf.math.reduce_prod(o, axis=1, name='reduce_prod')
 
         if include_divide:
-            o = Lambda(lambda t: tf.math.divide(t, 7), name='divide')(o)
+            o = tf.math.divide(o, 7, name='divide',)
+            o = tf.cast(o, 'float32', name='cast_f32')
 
-        o = Lambda(lambda t: tf.expand_dims(t, axis=1), name='expand')(o)
+        o = Lambda(lambda o: tf.expand_dims(o, axis=1), name='expand')(o)
 
         check_model = Model(inputs=inputs, outputs=o, name=f'{gen_name}_{mod_name}')
 
@@ -217,18 +245,21 @@ if __name__ == '__main__':
         ry = r(i_y)
         # rx / ry
         o = d(dict(x=rx, y=ry))
-        # rx^2 / ry
-        o = m(dict(x=o, y=rx))
-        # sum(rx^2/ry)
-        so = s(o)
-        # rx^2 * sum(rx^2/ry) / ry
-        o = m(dict(x=o, y=so))
-        # rx^2 * sum(rx^2/ry) / ry^2
-        o = d(dict(x=o, y=ry))
-        # sum(rx^2 * sum(rx^2/ry) / ry^2)
-        so = s(o)
-        # [rx^2 * sum(rx^2/ry)] / [ry^2 * sum(rx^2 * sum(rx^2/ry) / ry^2)]
-        o = d(dict(x=o, y=so))
+
+        for i in range(pa.slow_repeat):
+            # rx^2 / ry
+            o = m(dict(x=o, y=rx))
+            # sum(rx^2/ry)
+            so = s(o)
+            # rx^2 * sum(rx^2/ry) / ry
+            o = m(dict(x=o, y=so))
+            # rx^2 * sum(rx^2/ry) / ry^2
+            o = d(dict(x=o, y=ry))
+            # sum(rx^2 * sum(rx^2/ry) / ry^2)
+            so = s(o)
+            # [rx^2 * sum(rx^2/ry)] / [ry^2 * sum(rx^2 * sum(rx^2/ry) / ry^2)]
+            o = d(dict(x=o, y=so))
+
         o = s(o)
         o = Lambda(lambda t: t, name='output')(o)
 
