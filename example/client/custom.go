6f0ca6154039ae4718300c4a87c9d45a89739e46
David Choi <dchoi@viantinc.com>
2023-09-18 12:55:41 -0700

    Fix bug with custom registry.

--- example/client/custom.go
+++ example/client/custom.go
@@ -5,6 +5,10 @@ type customMakerRegistry struct {
 }
 
 func (c *customMakerRegistry) Register(k string, gen func() interface{}) (old func() interface{}, ok bool) {
+	if c.registry == nil {
+		c.registry = make(map[string]func() interface{})
+	}
+
 	old, ok = c.registry[k]
 	c.registry[k] = gen
 
